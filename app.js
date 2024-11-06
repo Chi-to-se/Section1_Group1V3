a93207038d9caa93906465cbd717b88bd1bd94e7
Chitipat Pangma <92365933+Chi-to-se@users.noreply.github.com>
2024-11-07 00:53:13 +0700

    Add Navbar.html
    - Add .env file
    - Add navbar.html
    - Change folder html to public
    - Add static middleware images(for bug images doesn't show)
    
    Todo:
    - Make Navbar appear in every html files
    - Add home page

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const path = require('path');
 const dotenv = require('dotenv');
 const mysql = require('mysql2');
 
-
-const port = 3030;
 const app = express();
 const router = express.Router();
 
@@ -18,24 +16,31 @@ router.use(express.urlencoded({ extended : true }));
 
 
 app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 router.get('/', (req,res) => 
     {
         console.log(`Request at ${req.originalUrl}`);
-        res.sendFile(path.join(`${__dirname}/html/index.html`));
+        res.sendFile(path.join(`${__dirname}/public/index.html`));
     }
 )
 
 router.get('/login', (req,res) => 
     {
         console.log(`Request at ${req.originalUrl}`);
-        res.sendFile(path.join(`${__dirname}/html/login.html`));
+        res.sendFile(path.join(`${__dirname}/public/login.html`));
+    }
+)
+
+router.get('/navbar', (req,res) => 
+    {
+        console.log(`Request at ${req.originalUrl}`);
+        res.sendFile(path.join(`${__dirname}/public/navbar.html`));
     }
 )
 
 
-
-
+const port = process.env.port;
 app.listen(port, () => 
     {
         console.log(`Server listening on port: ${port}`);
